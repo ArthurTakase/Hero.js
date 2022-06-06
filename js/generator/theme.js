534737b42221ee563102c2f8e70bd97955bb7eb9
arthurtakase <arthur.decaen@epitech.eu>
2022-06-06 23:39:23 +0200

    FEAT: add light theme

---
+++ js/generator/theme.js
@@ -0,0 +1,21 @@
+const icon = document.getElementById("toggleTheme");
+
+function switchTheme() {
+    if (localStorage.getItem("theme") === "theme-dark") {
+        localStorage.setItem("theme", "theme-light");
+        document.documentElement.className = "theme-light";
+        icon.innerHTML = '<i class="bx bx-moon nav__icon" ></i><span class="nav__name">Theme</span>';
+    } else {
+        localStorage.setItem("theme", "theme-dark");
+        document.documentElement.className = "theme-dark";
+        icon.innerHTML = '<i class="bx bx-sun nav__icon" ></i><span class="nav__name">Theme</span>';
+    }
+}
+
+(function() {
+    if (localStorage.getItem('theme') === 'theme-dark') {
+        switchTheme('theme-dark');
+    } else {
+        switchTheme('theme-light');
+    }
+})();
