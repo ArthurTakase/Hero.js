6f734511fbf185c501486edd26c833b711088397
arthurtakase <arthur.decaen@epitech.eu>
2022-09-27 23:28:58 +0200

    FEAT: add delete option for localStorage

--- js/generator/misc/localStorage.js
+++ js/generator/misc/localStorage.js
@@ -10,6 +10,8 @@ function save() {
     localGame[game.gameInfos.title] = game
     localStorage.setItem("games", JSON.stringify(localGame))
 
+    createNotif(`Saved as '${game.gameInfos.title}'`, "<i class='bx bx-info-circle'></i> Infos")
+
     searchLocalGame()
 }
 
@@ -17,7 +19,7 @@ function searchLocalGame() {
     var json = JSON.parse(localStorage.getItem("games"))
     console.log(json)
     loadSection.innerHTML = ""
-    if (json == null) {
+    if (json == null || Object.keys(json).length === 0) {
         localGame = {}
         loadContainer.style.display = "none"
     } else {
@@ -29,12 +31,13 @@ function searchLocalGame() {
 
 function addLocalGame(json) {
     console.log(json)
-    loadSection.innerHTML += `<a class="homeGame" onclick="load('${json.gameInfos.title}')">
-                                <img src="${json.gameInfos.previewPicture}" alt="">
-                                <div class="homeGameAll">
+    loadSection.innerHTML += `<a class="homeGame">
+                                <img src="${json.gameInfos.previewPicture}" onclick="load('${json.gameInfos.title}')">
+                                <div class="homeGameAll" onclick="load('${json.gameInfos.title}')">
                                     <div class="homeGameText">${json.gameInfos.title}</div>
                                     <div class="homeGameDate">${json.gameInfos.previewDate}</div>
                                 </div>
+                                <div class="homeGameDelete" onclick="removeFromLocalGame('${json.gameInfos.title}')"><i class='bx bxs-trash'></i></div>
                             </a>`
 }
 
@@ -48,6 +51,12 @@ function load(name) {
     } catch (e) {}
 }
 
+function removeFromLocalGame(name) {
+    createNotif(`'${name}' deleted`, "<i class='bx bx-info-circle'></i> Infos")
+    delete localGame[name]
+    localStorage.setItem("games", JSON.stringify(localGame))
+    searchLocalGame()
+}
 
 searchLocalGame()
     // localStorage.clear();
