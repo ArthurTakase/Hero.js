ba5b88f38d7713d7541b7641c0751fa85f9e2370
ArthurTakase <arthur.decaen@epitech.eu>
2023-07-26 11:01:36 +0200

    FEAT: possibilité de changer le background des dialogues

--- src/Components/Select.jsx
+++ src/Components/Select.jsx
@@ -1,9 +1,10 @@
 import { states, txt } from '../App'
 
-export default function Select(props, type) {
+export default function FullpageSelect(props, type, onClick_callback) {
+  const elementLength = Object.entries(props).length
   const elements = Object.entries(props).map((element, index) => {
     return (
-      <button className='select_element' value={element[0]} key={index}>
+      <button onClick={() => onClick_callback(element)} className='select_element' key={index}>
         {type === 'img' ? <img src={element[1]} alt={element[0]} /> : element[0]}
       </button>
     )
@@ -12,7 +13,16 @@ export default function Select(props, type) {
   states.set.setModalContent(
     <div className="modalContent">
       <div className="modalTitle">{txt('misc.pleaseSelect')}</div>
-      {elements}
+      <div className="localGames">
+        {elementLength ? 
+        <>
+          <button onClick={() => onClick_callback(null)} className='select_element'>
+            {txt('misc.remove')}
+          </button>
+          {elements}
+        </>
+        : txt('misc.is_empty')}
+      </div>
     </div>
   )
   states.set.setOpen(true)
--- src/Pages/Dialog.jsx
+++ src/Pages/Dialog.jsx
@@ -1,25 +1,36 @@
 import Header from '../Components/Header'
 import { data, txt, refs, states } from '../App'
-import Select from '../Components/Select'
+import FullpageSelect from '../Components/Select'
 import '../scss/window_behavior.scss'
 
 import { useState, useEffect } from 'react'
 
 export default function Dialog() {
-  const [background, setBackground] = useState(<>
-    <button onClick={() => Select(data.data.pictures, 'img')}>+</button>
-  </>)
+  const [background, setBackground] = useState(<></>)
+  const [btnBackground, setBtnBackground] = useState(
+    <button data-tooltip={txt('dialog.setBackground')} className="btnBackground"
+      onClick={() => FullpageSelect(data.data.pictures, 'img', (element) => {
+        if (element === null) setBackground(<></>)
+        else setBackground(<img className="dialogBackground" src={element[1]} alt={element[0]}></img>)
+        states.set.setOpen(false)
+        // TODO: Mettre à jour les data du dialog
+      })}
+    ></button>
+  )
   
   useEffect(() => {
     states.get.background = background
     states.set.background = setBackground
-  }, [background, setBackground])
+    states.get.btnBackground = btnBackground
+    states.set.btnBackground = setBtnBackground
+  }, [background, setBackground, btnBackground, setBtnBackground])
 
   return (
     <>
     <Header title={txt('dialog.title')} />
     <div className="dialog content" ref={refs.window.dialog}>
       {background}
+      {btnBackground}
     </div>
     </>
   )
